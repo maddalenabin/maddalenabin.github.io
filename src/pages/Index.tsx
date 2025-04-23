2bf659034108a059f119df56c840244e3085a7f8
maddalenabin <56296515+maddalenabin@users.noreply.github.com>
2025-04-23 23:57:17 +0200

    Create Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,16 @@
+
+import { Hero } from "@/components/Hero";
+import { Projects } from "@/components/Projects";
+import { Experience } from "@/components/Experience";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Hero />
+      <Projects />
+      <Experience />
+    </div>
+  );
+};
+
+export default Index;
